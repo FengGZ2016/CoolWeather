7ba066ddbc83b60f69af68c11e46b6f12a65a860
冯国富 <fgz2016@outlook.com>
2017-05-17 15:20:33 +0800

    运用引用布局完成显示天气的布局

---
+++ app/src/main/java/com/example/administrator/coolweather/ui/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.coolweather.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.coolweather.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
